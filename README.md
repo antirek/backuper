bd8bdb4c3611a29988d607402375f43774b11104
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-06-14 23:39:19 +0700

    up version change readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # backuper
+
 backup config tool for db mysqldump, mongodump, pg_dump
+
+
+## install 
+
+require python2, pip
+
+> pip install backuper
+
+
+## config
+
+- edit /etc/backuper.conf 
+
+- add database config files to /etc/backuper/databases
+
+- add cron task 
+
+file /etc/crontab
+
+> 10 1    * * *   root    /usr/local/bin/backuper -c /etc/backuper
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='backuper',
-    version='0.1',
+    version='0.1.2',
     author='Denis Krashnikov',
     author_email='mgpwanderer@gmail.com',
     maintainer='Sergey Dmitriev',
     maintainer_email='serge.dmitriev@gmail.com',
     packages=['backuper', ],
     url='https://github.com/antirek/backuper',
-    download_url='https://github.com/antirek/backuper/archive/0.1.tar.gz',
+    download_url='https://github.com/antirek/backuper/archive/0.1.2.tar.gz',
     scripts=['bin/backuper', ],
     data_files=[
         ('/etc/backuper', ['config/backuper.conf']),
